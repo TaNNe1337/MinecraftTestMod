a77585aeabf5b62131fd7e06601c7a72b9a15282
Niels <Niels@DESKTOP-O5DO0NS>
2018-05-27 00:36:56 +0200

    added saphire

--- MDKExample/src/main/java/com/TaNNe/TestMod/init/ModItems.java
+++ MDKExample/src/main/java/com/TaNNe/TestMod/init/ModItems.java
@@ -12,5 +12,6 @@ public class ModItems {
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 
 	public static final Item RUBY = new ItemBase("ruby");
+	public static final Item SAPHIRE = new ItemBase("saphire");
 
 }
